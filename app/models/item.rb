90134d2a98afdad548ac24461c4765864be3b61f
ikasiromii28 <camelliakamesan716@gmail.com>
2021-12-21 16:23:44 +0900

    バリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,7 @@ class Item < ApplicationRecord
     validates :shipping_id,numericality: { other_than: 1 , message: "can't be blank"}
     validates :prefecture_id,numericality: { other_than: 1 , message: "can't be blank"}
     validates :schedule_id,numericality: { other_than: 1 , message: "can't be blank"}
-    validates :price
-    validates :user
+    validates :price,numericality:{only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
   end
 
