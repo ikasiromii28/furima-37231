c667058c543993a29aa93c76ec4e048fe6570c9f
ikasiromii28 <camelliakamesan716@gmail.com>
2021-12-21 18:00:03 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
+    @items = Item.all
   end
 
   def new
@@ -7,12 +9,21 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id,
-       :status_id, :shipping_id, :prefecture_id, :schedule_id, :price, :user, :image)
-    end
+       :status_id, :shipping_id, :prefecture_id, :schedule_id, :price, :image).
+       merge(user_id: current_user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,14 +35,17 @@ ActiveRecord::Schema.define(version: 2021_12_21_013901) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.text "info", null: false
+    t.text "description", null: false
     t.integer "category_id", null: false
     t.integer "status_id", null: false
     t.integer "shipping_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "schedule_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
