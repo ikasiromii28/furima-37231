833297cae35192f317af7c5ba911d927e02b5398
ikasiromii28 <camelliakamesan716@gmail.com>
2021-12-28 11:21:33 +0900

    購入画面の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :find_item, only: [:index]
+
   def index
+  end
+
+  private
+  def find_item
+    @item = Item.find(params[:item_id])
   end
 end
