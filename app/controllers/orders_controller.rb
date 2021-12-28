737f5bdfd67a74016317207836e62e261216b197
ikasiromii28 <camelliakamesan716@gmail.com>
2021-12-28 20:45:09 +0900

    購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,41 @@
 class OrdersController < ApplicationController
-  before_action :find_item, only: [:index]
+  before_action :authenticate_user!
+  before_action :find_item, only: [:index, :create]
 
   def index
+    @order_buyer = OrderBuyer.new
+    redirect_to root_path if @item.user_id == current_user.id #|| !@item.order.presence?
+  end
+
+  def create
+    @order_buyer = OrderBuyer.new(order_params)
+    if @order_buyer.valid?
+      pay_item
+      @order_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
+
+  def order_params
+    params.require(:order_buyer).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(
+      token: params[:token], item_id: params[:item_id], user_id: current_user.id
+    )
+  end
+
   def find_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
---
+++ app/models/order_buyer.rb
@@ -0,0 +1,22 @@
+class OrderBuyer
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id, :token
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'の3桁目と4桁目の間にハイフンを入力してください (例： 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10}\z|\A\d{11}\z/, message: 'は10桁以上、11桁以下で入力してください' }
+    validates :user_id
+    validates :item_id
+  end
+  validates :phone_number,numericality: {message: 'は半角数値で入力してください'}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Buyer.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses,
+                 building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
